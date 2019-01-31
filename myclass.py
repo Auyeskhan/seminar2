f537407d51f5056f1367b4c111e258a41d6bb5e4
Auyeskhan <rauan.sdu.93@gmail.com>
2019-01-31 18:19:28 +0500

    I edited

--- myclass.py
+++ myclass.py
@@ -1,9 +1,16 @@
-class my_class:
-	def __init__(self,surname,city,profession)
+class MyClass:
+	
+	def __init__(self,surname,city,profession):
 		self.surname = surname
 		self.city = city
 		self.profession = profession
 	def meth_1(self):
-		print(a.surname)
-		print(a.city)
-		print(a.profession)	
+		print(self.surname)
+		print(self.city)
+		print(self.profession)
+
+	def meth_2(self):
+		self.surname = "Yessenbayev"
+		self.city = "Astana"
+		self.profession = "teacher"
+
