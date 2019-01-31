f206091289689fa37a138fbb171b5c1e552d841e
Auyeskhan <rauan.sdu.93@gmail.com>
2019-01-31 10:39:32 +0500

    I added myclass.py

---
+++ myclass.py
@@ -0,0 +1,9 @@
+class my_class:
+	def __init__(self,surname,city,profession)
+		self.surname = surname
+		self.city = city
+		self.profession = profession
+	def meth_1(self):
+		print(a.surname)
+		print(a.city)
+		print(a.profession)	
