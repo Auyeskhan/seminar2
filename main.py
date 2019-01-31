2f0a444a6aa66213129e7685f903c3ef26c42f6a
Auyeskhan <rauan.sdu.93@gmail.com>
2019-01-31 10:59:28 +0500

    I edited main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from my_info import my_class
+class my_class:
+	def meth_2(self):
+		self.surname = "Yessenbayev"
+		self.city = "Astana"
+		self.profession = "teacher"
 
 a = my_class("Auyeskhan","Qostanay","teacher")
+a.meth_2()
