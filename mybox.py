d015b110fb81eb24527b821eac6ded81b9e4b2ab
Auyeskhan <47101127+Auyeskhan@users.noreply.github.com>
2019-02-04 03:26:01 +0600

    Update mybox.py

--- mybox.py
+++ mybox.py
@@ -12,18 +12,4 @@ class MyBox:
   def __contains__(self,item):
     return item in self._theItems
   def __iter__(self,item):
-    return BagIterator (self._theItems)
-
-class BagIterator():
-  def __init__( self, L ):
-    self.L = L # the list
-    self.idx = 0 # curent item
-  def __iter__(self):
-    return self
-  def __next__(self):
-    if self.idx < len(self.L):
-      item = self.L[self.idx]
-      self.idx += 1
-      return item
-    else:
-      raise StopIteration
+    return MyBoxIterator (self._theItems)
