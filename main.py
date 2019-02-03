004675a9db9258d4848d35b038b1790d78a18c73
Auyeskhan <47101127+Auyeskhan@users.noreply.github.com>
2019-02-04 03:42:10 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from mybox import MyBox
+from myboxiterator import MyBoxIteator
+
+b = MyBox()
+b.add('hello')
+b.add(1)
+b.add(', ')
+b.add(True)
+b.add('Bob')
+b.remove(True)
+if(1 in b) and (len(b)>0):
+  b.remove(1)
+msg = ''
+for bg in b:
+  msg += bg
+print(msg)
