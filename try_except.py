6b16134282b0901392f7dac0a096f457d25d1158
Auyeskhan <47101127+Auyeskhan@users.noreply.github.com>
2019-02-04 01:56:05 +0600

    Update try_except.py

--- try_except.py
+++ try_except.py
@@ -5,6 +5,26 @@ def openfl(x):
       read_data = file.read()
   except:
     print('Ouch, Could not open file.log')
+#IndexError
+def lst(a):
+  try:
+    return abc[a]
+  except:
+    print("Out of index")
+
+#KeyError
+def keyer(b):
+  try:
+    return cba[b]
+  except:
+    print("key is not found in a dictionary")
   
 if __name__ == '__main__':
+# FileNotFoundError
   print(openfl('file.log'))
+#IndexError
+  abc=[1,2,3]
+  print(lst(3))
+#KeyError
+  cba = { 'a':1, 'b':2 }
+  print (keyer('c'))
