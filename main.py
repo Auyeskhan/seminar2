240e47476bde4ae173256c4e33303ed996ad3ce3
Auyeskhan <47101127+Auyeskhan@users.noreply.github.com>
2019-02-08 12:12:21 +0600

    added file

---
+++ main.py
@@ -0,0 +1,11 @@
+from mybox import MyBox
+from myboxiterator import MyBoxIterator
+from myclass import MyClass
+b = MyBox()
+b.add(MyClass('Auyeskhan', 'Kostanay', 'Teacher'))
+b.add(MyClass('John', 'London', 'Writer'))
+b.add(MyClass('Shamel', 'Kostanay', 'Student'))
+
+b.remove(1)
+for i in b:
+      i.meth_1()
---
+++ myclass.py
@@ -0,0 +1,20 @@
+class MyClass:
+	_Id = 0
+	def __init__(self,surname,city,profession):
+                MyClass._Id += 1
+                self._cId = MyClass._Id
+                self.surname = surname
+                self.city = city
+                self.profession = profession
+	def meth_1(self):
+                print("ID:", self._cId)
+                print("Surname: ",self.surname)
+                print("City: ", self.city,)
+                print("Profession", self.profession)
+                print('------------------')
+
+	def meth_2(self):
+		self.surname = "Yessenbayev"
+		self.city = "Astana"
+		self.profession = "teacher"
+
